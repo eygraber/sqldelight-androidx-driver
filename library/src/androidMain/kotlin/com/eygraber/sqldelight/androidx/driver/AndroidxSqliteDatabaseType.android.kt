356c1ee61343aed1bed6edf63baf07789d3f7af6
Eliezer Graber <me@eygraber.com>
2025-02-21 12:12:00 -0800

    AndroidxSqliteDatabaseType.File helpers should be extensions on the companion (#22)

--- library/src/androidMain/kotlin/com/eygraber/sqldelight/androidx/driver/AndroidxSqliteDatabaseType.android.kt
+++ library/src/androidMain/kotlin/com/eygraber/sqldelight/androidx/driver/AndroidxSqliteDatabaseType.android.kt
@@ -3,11 +3,11 @@ package com.eygraber.sqldelight.androidx.driver
 import android.content.Context
 import java.io.File as JavaFile
 
-public fun AndroidxSqliteDatabaseType.File(
+public fun AndroidxSqliteDatabaseType.Companion.File(
   context: Context,
   name: String,
 ): AndroidxSqliteDatabaseType.File = AndroidxSqliteDatabaseType.File(context.getDatabasePath(name).absolutePath)
 
-public fun AndroidxSqliteDatabaseType.File(
+public fun AndroidxSqliteDatabaseType.Companion.File(
   file: JavaFile,
 ): AndroidxSqliteDatabaseType.File = AndroidxSqliteDatabaseType.File(file.absolutePath)
--- library/src/jvmMain/kotlin/com/eygraber/sqldelight/androidx/driver/AndroidxSqliteDatabaseType.jvm.kt
+++ library/src/jvmMain/kotlin/com/eygraber/sqldelight/androidx/driver/AndroidxSqliteDatabaseType.jvm.kt
@@ -2,6 +2,6 @@ package com.eygraber.sqldelight.androidx.driver
 
 import java.io.File as JavaFile
 
-public fun AndroidxSqliteDatabaseType.File(
+public fun AndroidxSqliteDatabaseType.Companion.File(
   file: JavaFile,
 ): AndroidxSqliteDatabaseType.File = AndroidxSqliteDatabaseType.File(file.absolutePath)
