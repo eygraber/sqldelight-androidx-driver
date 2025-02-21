424b1a62bd8bfea7d775dcf915a2f6bf789fa9db
Eliezer Graber <me@eygraber.com>
2025-02-21 15:06:43 -0500

    AndroidxSqliteDatabaseType.File helpers should be extensions on the companion

--- library/src/commonMain/kotlin/com/eygraber/sqldelight/androidx/driver/AndroidxSqliteDatabaseType.kt
+++ library/src/commonMain/kotlin/com/eygraber/sqldelight/androidx/driver/AndroidxSqliteDatabaseType.kt
@@ -4,4 +4,6 @@ public sealed interface AndroidxSqliteDatabaseType {
   public data class File(val databaseFilePath: String) : AndroidxSqliteDatabaseType
   public data object Memory : AndroidxSqliteDatabaseType
   public data object Temporary : AndroidxSqliteDatabaseType
+
+  public companion object
 }
