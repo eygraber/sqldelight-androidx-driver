98c3ec20fa7f58a8a926281355e21b20acde021e
Eliezer Graber <me@eygraber.com>
2025-05-19 21:33:11 +0000

    Remove statement cache ThreadLocal (#63)
    - There was a bug where there could be concurrent modification of the global view of all caches
      - While fixing that I realized that the SQLiteStatements can be used across threads as long as it isn't concurrently
      - Removing the ThreadLocal simplifies the implementation and removes the reported bug
    
    Fixes #61

--- library/src/nativeMain/kotlin/com/eygraber/sqldelight/androidx/driver/ThreadLocalId.kt
+++ library/src/nativeMain/kotlin/com/eygraber/sqldelight/androidx/driver/ThreadLocalId.kt
@@ -3,6 +3,7 @@ package com.eygraber.sqldelight.androidx.driver
 import kotlin.concurrent.AtomicInt
 
 internal object ThreadLocalId {
-  val id = AtomicInt(0)
+  private val id = AtomicInt(0)
+
   fun next(): Int = id.incrementAndGet()
 }
